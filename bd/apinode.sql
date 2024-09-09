ba9f4fc3a38e9c71de7e83c22e2c874a7c4bc834
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2024-09-08 22:36:58 -0500

    creating the database

--- bd/apinode.sql
+++ bd/apinode.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-09-2024 a las 05:54:04
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 09-09-2024 a las 04:47:31
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,43 +24,458 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Estructura de tabla para la tabla `aplicaciones_trabajos`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `aplicaciones_trabajos` (
   `id` int(11) NOT NULL,
-  `username` text NOT NULL,
-  `email` text NOT NULL,
-  `password` text NOT NULL,
-  `createDate` timestamp NULL DEFAULT NULL
+  `id_trabajo` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `estado` enum('pendiente','aceptado','rechazado','retirado') DEFAULT 'pendiente',
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fecha_actualizacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `users`
+-- Estructura de tabla para la tabla `calificaciones`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `createDate`) VALUES
-(199, 'lcchaverra', 'lcchaverra@gmail.com', '123456', '0000-00-00 00:00:00');
+CREATE TABLE `calificaciones` (
+  `id` int(11) NOT NULL,
+  `id_trabajo` int(11) NOT NULL,
+  `id_empleador` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `calificacion` int(11) NOT NULL CHECK (`calificacion` >= 1 and `calificacion` <= 5),
+  `comentario` text DEFAULT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `id_trabajo` int(11) NOT NULL,
+  `id_empleador` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_empleos`
+--
+
+CREATE TABLE `estado_empleos` (
+  `id` int(11) NOT NULL,
+  `nombre` enum('abierto','en_progreso','cerrado') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_usuarios`
+--
+
+CREATE TABLE `estado_usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` enum('activo','inactivo') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habilidades`
+--
+
+CREATE TABLE `habilidades` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habilidades_usuarios`
+--
+
+CREATE TABLE `habilidades_usuarios` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_habilidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id` int(11) NOT NULL,
+  `id_remitente` int(11) NOT NULL,
+  `id_receptor` int(11) NOT NULL,
+  `id_trabajo` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesiones`
+--
+
+CREATE TABLE `profesiones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `nombre` enum('empleador','empleado') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `trabajos`
+--
+
+CREATE TABLE `trabajos` (
+  `id` int(11) NOT NULL,
+  `id_empleador` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `id_ubicacion` int(11) NOT NULL,
+  `tipo_pago` enum('por_hora','fijo') NOT NULL,
+  `tarifa_por_hora` decimal(10,2) DEFAULT NULL,
+  `rango_precio` varchar(50) DEFAULT NULL,
+  `id_estado` int(11) DEFAULT 1,
+  `id_trabajador_asignado` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fecha_actualizacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ubicaciones`
+--
+
+CREATE TABLE `ubicaciones` (
+  `id` int(11) NOT NULL,
+  `ciudad` varchar(255) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  `pais` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo_electronico` varchar(255) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `profesion` varchar(255) DEFAULT NULL,
+  `experiencia` text DEFAULT NULL,
+  `educacion` text DEFAULT NULL,
+  `tarifa_por_hora` decimal(10,2) DEFAULT NULL,
+  `estado` enum('activo','inactivo') DEFAULT 'activo',
+  `rol` enum('empleador','empleado') NOT NULL,
+  `foto_perfil` varchar(255) DEFAULT NULL,
+  `informacion_adicional` text DEFAULT NULL,
+  `calificacion` decimal(3,2) DEFAULT 0.00,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fecha_actualizacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_aplicados_trabajos`
+--
+
+CREATE TABLE `usuarios_aplicados_trabajos` (
+  `id` int(11) NOT NULL,
+  `id_trabajo` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `estado` enum('pendiente','aceptado','rechazado') DEFAULT 'pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `users`
+-- Indices de la tabla `aplicaciones_trabajos`
 --
-ALTER TABLE `users`
+ALTER TABLE `aplicaciones_trabajos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_trabajo` (`id_trabajo`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- Indices de la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_trabajo` (`id_trabajo`),
+  ADD KEY `id_empleador` (`id_empleador`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_trabajo` (`id_trabajo`),
+  ADD KEY `id_empleador` (`id_empleador`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- Indices de la tabla `estado_empleos`
+--
+ALTER TABLE `estado_empleos`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estado_usuarios`
+--
+ALTER TABLE `estado_usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `habilidades`
+--
+ALTER TABLE `habilidades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `habilidades_usuarios`
+--
+ALTER TABLE `habilidades_usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_habilidad` (`id_habilidad`);
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_remitente` (`id_remitente`),
+  ADD KEY `id_receptor` (`id_receptor`),
+  ADD KEY `id_trabajo` (`id_trabajo`);
+
+--
+-- Indices de la tabla `profesiones`
+--
+ALTER TABLE `profesiones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `trabajos`
+--
+ALTER TABLE `trabajos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empleador` (`id_empleador`),
+  ADD KEY `id_ubicacion` (`id_ubicacion`),
+  ADD KEY `id_estado` (`id_estado`),
+  ADD KEY `id_trabajador_asignado` (`id_trabajador_asignado`);
+
+--
+-- Indices de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo_electronico` (`correo_electronico`);
+
+--
+-- Indices de la tabla `usuarios_aplicados_trabajos`
+--
+ALTER TABLE `usuarios_aplicados_trabajos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_trabajo` (`id_trabajo`),
+  ADD KEY `id_empleado` (`id_empleado`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT de la tabla `aplicaciones_trabajos`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=200;
+ALTER TABLE `aplicaciones_trabajos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_empleos`
+--
+ALTER TABLE `estado_empleos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_usuarios`
+--
+ALTER TABLE `estado_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `habilidades`
+--
+ALTER TABLE `habilidades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `habilidades_usuarios`
+--
+ALTER TABLE `habilidades_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `profesiones`
+--
+ALTER TABLE `profesiones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `trabajos`
+--
+ALTER TABLE `trabajos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_aplicados_trabajos`
+--
+ALTER TABLE `usuarios_aplicados_trabajos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `aplicaciones_trabajos`
+--
+ALTER TABLE `aplicaciones_trabajos`
+  ADD CONSTRAINT `aplicaciones_trabajos_ibfk_1` FOREIGN KEY (`id_trabajo`) REFERENCES `trabajos` (`id`),
+  ADD CONSTRAINT `aplicaciones_trabajos_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD CONSTRAINT `calificaciones_ibfk_1` FOREIGN KEY (`id_trabajo`) REFERENCES `trabajos` (`id`),
+  ADD CONSTRAINT `calificaciones_ibfk_2` FOREIGN KEY (`id_empleador`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `calificaciones_ibfk_3` FOREIGN KEY (`id_empleado`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_trabajo`) REFERENCES `trabajos` (`id`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_empleador`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `comentarios_ibfk_3` FOREIGN KEY (`id_empleado`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `habilidades_usuarios`
+--
+ALTER TABLE `habilidades_usuarios`
+  ADD CONSTRAINT `habilidades_usuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `habilidades_usuarios_ibfk_2` FOREIGN KEY (`id_habilidad`) REFERENCES `habilidades` (`id`);
+
+--
+-- Filtros para la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`id_remitente`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `mensajes_ibfk_2` FOREIGN KEY (`id_receptor`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `mensajes_ibfk_3` FOREIGN KEY (`id_trabajo`) REFERENCES `trabajos` (`id`);
+
+--
+-- Filtros para la tabla `trabajos`
+--
+ALTER TABLE `trabajos`
+  ADD CONSTRAINT `trabajos_ibfk_1` FOREIGN KEY (`id_empleador`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `trabajos_ibfk_2` FOREIGN KEY (`id_ubicacion`) REFERENCES `ubicaciones` (`id`),
+  ADD CONSTRAINT `trabajos_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `estado_empleos` (`id`),
+  ADD CONSTRAINT `trabajos_ibfk_4` FOREIGN KEY (`id_trabajador_asignado`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `usuarios_aplicados_trabajos`
+--
+ALTER TABLE `usuarios_aplicados_trabajos`
+  ADD CONSTRAINT `usuarios_aplicados_trabajos_ibfk_1` FOREIGN KEY (`id_trabajo`) REFERENCES `trabajos` (`id`),
+  ADD CONSTRAINT `usuarios_aplicados_trabajos_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
