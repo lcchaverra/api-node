efe845728a8910a29af504328ac9eff92b99ad96
lcchaverrahelpp <lcchaverra@helppeoplecloud.com>
2024-11-19 00:34:22 -0500

    feat: se agrega documentación con swagger

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 require('dotenv').config()
 const users = require("./routes/users/userRoutes");
 const port = process.env.PORT || 3000;
 const cors = require('cors')
 
-//middlweares genrales
-app.use(cors);
+//middlweares generales
+app.use(cors());
 app.use(express.json());
 
+// Middleware para Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 //Rutas principales
-app.use('/users',users)
+app.use('/api/users',users)
 
 //Ruta base
 app.get('/', (req, res) =>  {
@@ -19,4 +24,5 @@ app.get('/', (req, res) =>  {
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
+    console.log(`Documentación disponible en http://localhost:${port}/api-docs`);
 })
